b6ebc75353dffaab938d48b7335c073b812150f6
awcheung1213 <awcheung1213@gmail.com>
2023-01-09 23:52:48 -0800

    complete fibonacci

---
+++ js-algorithms/sum-possible.ts
@@ -0,0 +1,11 @@
+/*
+Write a function sum_possible that takes in an amount and a list of positive numbers. The function should return a boolean indicating whether or not it is possible to create the amount by summing numbers of the list. You may reuse numbers of the list as many times as necessary.
+
+You may assume that the target amount is non-negative.
+*/
+
+function sumPossible(amount: number, list: Array<number>): number{
+  return 0
+}
+
+module.exports = sumPossible;
